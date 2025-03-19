00391f6ae2b59702cfbf34b3f47050109ea6c35c
Kasim Ke <shkpostisi@esimerkki.com>
2025-03-19 15:21:23 +0200

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,10 +7,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/urheiluEtu">
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter basename="/urheiluEtu">
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>
 );
