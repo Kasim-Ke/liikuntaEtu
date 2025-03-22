0d6936dab62f9281b0424dfa5fe6ec784ae275fb
Kasim Ke <shkpostisi@esimerkki.com>
2025-03-22 16:16:28 +0200

    look

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import AuthRedirect from "./components/Auth/AuthRedirect";
-import CompleteProfile from "./components/Profile/CompleteProfile";
-
-import Auth from "./components/Auth/Auth";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
-import Home from "./components/Home/Home";
-import LogDailyKilometers from "./components/UserData/LogDailyKilometers";
 
 function App() {
   return (
     <>
       <Navbar />
-      <AuthRedirect />
-      {/* Tämä suoritetaan aina, kun käyttäjä on kirjautunut sisään */}
       <Routes>
-        {/* Auth */}
-        <Route path="/auth" element={<Auth />} />
-
-        {/* Home Page */}
-        <Route path="/" element={<Home />} />
-
-        {/* Profile Completion Page */}
-        <Route path="/complete-profile" element={<CompleteProfile />} />
-
-        {/* Leaderboard Page */}
         <Route path="/leaderboard" element={<Leaderboard />} />
-        {/* userdata Page */}
-        <Route path="/logdaykilomeneters" element={<LogDailyKilometers />} />
       </Routes>
     </>
   );
--- src/components/Home/Home.jsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <div className="text-6xl text-red-700">Home</div>;
-};
-
-export default Home;
